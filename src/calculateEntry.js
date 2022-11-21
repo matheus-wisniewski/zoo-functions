119c35260df61572bea8ec3ad1342790c2a41dd4
Matheus <maahwiski@gmail.com>
2022-11-21 18:37:08 -0300

    requisito 8. Esse foi divertido

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,32 @@
 const data = require('../data/zoo_data');
 
-function countEntrants(entrants) {
-  //
-}
+const { prices } = data;
 
-function calculateEntry(entrants) {
-  // seu código aqui
-}
+const countEntrants = (arr) => {
+  const counter = {
+    child: 0,
+    adult: 0,
+    senior: 0,
+  };
+  arr.forEach((entrant) => {
+    if (entrant.age < 18) {
+      counter.child += 1;
+    } else if (entrant.age >= 18 && entrant.age < 50) {
+      counter.adult += 1;
+    } else {
+      counter.senior += 1;
+    }
+  });
+  return counter;
+};
+
+const calculateEntry = (entrants) => {
+  if (!entrants || !entrants.length) {
+    return 0;
+  }
+  const { child, adult, senior } = countEntrants(entrants);
+  const { child: childPrice, senior: seniorPrice, adult: adultPrice } = prices;
+  return child * childPrice + adult * adultPrice + senior * seniorPrice;
+};
 
 module.exports = { calculateEntry, countEntrants };
