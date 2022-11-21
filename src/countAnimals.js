ca4ae1c9462ffbc01770b3002f8b7f2cfbaf9564
Matheus <maahwiski@gmail.com>
2022-11-21 17:16:34 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,22 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
-}
+const { species } = data;
 
+const getSpecieAndSex = ({ specie, sex }) => {
+  const { residents } = species.find(({ name }) => name === specie);
+  if (sex) {
+    return residents.filter(({ sex: sexOf }) => sexOf === sex);
+  } return residents;
+};
+
+const countAllAnimals = () => species.reduce(
+  (animal, { name, residents }) => ({ ...animal, [name]: residents.length }), {},
+);
+
+const countAnimals = (animal) => {
+  if (animal) {
+    const capturedSpecieAndSex = getSpecieAndSex(animal);
+    return capturedSpecieAndSex.length;
+  } return countAllAnimals();
+};
 module.exports = countAnimals;
