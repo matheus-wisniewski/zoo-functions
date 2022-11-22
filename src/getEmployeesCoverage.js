d6537a2f6453ac619fc6b5ccd54b09c774b67bc0
Matheus <maahwiski@gmail.com>
2022-11-22 20:20:19 -0300

    mais um 90% do requisito. Amanhã o Renan vai tirar minha dúvida

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,20 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
-}
+const { employees } = data;
 
+const getEmployeesCoverage = ({ name, last, identificator }) => {
+  const { id, firstName, lastName, responsibleFor } = employees
+    .find((employee) =>
+      employee.name === name || employee.lastName === last || employee.id === identificator);
+
+  const infosOfEmployee = {
+    id,
+    fullName: `${firstName} ${lastName}`,
+    species: responsibleFor,
+    locations: responsibleFor.map((specie) => specie.location),
+  };
+  return infosOfEmployee;
+};
+
+console.log(getEmployeesCoverage('Stephanie'));
 module.exports = getEmployeesCoverage;
