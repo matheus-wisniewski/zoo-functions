3facd8c7afe473295c2e64bc0d3a8add9dd17e46
Matheus <maahwiski@gmail.com>
2022-11-22 18:58:06 -0300

    refatorado e agora sim finalizado requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -5,16 +5,12 @@ const { employees, species } = data;
 const getOldestFromFirstSpecies = (id) => {
   const getFirstSpecie = employees
     .find((employee) => employee.id === id).responsibleFor[0];
-
-  const getResiOfSpecies = species
+  const getResidentsBySpecie = species
     .filter((specie) => specie.id === getFirstSpecie)[0].residents;
-
-  const getOldest = getResiOfSpecies
+  const verifyTheOldest = getResidentsBySpecie
     .sort((a, b) => b.age - a.age)[0];
-
-  const arrOfOldest = [getOldest.name, getOldest.sex, getOldest.age];
-  return arrOfOldest;
+  const theOldest = [verifyTheOldest.name, verifyTheOldest.sex, verifyTheOldest.age];
+  return theOldest;
 };
 
-console.log(getOldestFromFirstSpecies('01422318-ca2d-46b8-b66c-3e9e188244ed'));
 module.exports = getOldestFromFirstSpecies;
