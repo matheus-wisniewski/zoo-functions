3d58756dc75dc87a585415b21eebc1802fa63005
Matheus <maahwiski@gmail.com>
2022-11-23 11:14:26 -0300

    finalizado requisito 12

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,3 +1,24 @@
 const getOpeningHours = require('../src/getOpeningHours');
 
-describe('Testes da função getOpeningHours', () => {});
+describe('Testes da função getOpeningHours', () => {
+  it('Verifica se a função ao receber Monday e 09:00-AM retorna a string esperada', () => {
+    expect(getOpeningHours('Monday', '09:00-AM')).toBe('The zoo is closed');
+  });
+  it('Verifica se a função ao receber Tuesday e 09:00-AM retorna a string esperada', () => {
+    expect(getOpeningHours('Tuesday', '09:00-AM')).toBe('The zoo is open');
+  });
+  it('Verifica se a função ao receber Wednesday e 09:00-AM retorna a string esperada', () => {
+    expect(getOpeningHours('Wednesday', '09:00-PM')).toBe('The zoo is closed');
+  });
+  it('Verifica se a função retorna o objeto esperado quando não recebe argumentos', () => {
+    expect(getOpeningHours()).toStrictEqual({
+      Tuesday: { open: 8, close: 6 },
+      Wednesday: { open: 8, close: 6 },
+      Thursday: { open: 10, close: 8 },
+      Friday: { open: 10, close: 8 },
+      Saturday: { open: 8, close: 10 },
+      Sunday: { open: 8, close: 8 },
+      Monday: { open: 0, close: 0 },
+    });
+  });
+});
